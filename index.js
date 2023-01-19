c62443545e88475686e0b8ce2a0383fbcf9047da
Amyxcode <107558599+Amyxcode@users.noreply.github.com>
2023-01-19 13:01:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
-let generateEL= document.getElementById("generate-el")
-let password1El=document.getElementById("password1-el")
-let password2El=document.getElementById("password2-el")
+setTimeout(function(){
+    modal.style.display = 'inline'
+}, 1500)
 
-function btn1() {
-    let randomIndexOne = (Math.floor(Math.random()* characters.length)
-    password1El.textContent=characters[randomIndexOne]
-    return 
-}
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
+}) 
+
+declineBtn.addEventListener('mouseenter', function(){
+    modalChoiceBtns.classList.toggle('modal-btns-reverse')
+}) 
+
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    
+    const consentFormData = new FormData(consentForm)
+    const fullName = consentFormData.get('fullName')
+    
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="loading.svg" class="loading">
+        <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>` 
+    
+    setTimeout(function(){
+        document.getElementById('upload-text').innerText = `
+        Making the sale...`
+    }, 1500)
+    
+    
+    setTimeout(function(){
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="kevin.gif">
+        </div>
+    `
+    modalCloseBtn.disabled = false
+    }, 3000)
+  
+}) 
