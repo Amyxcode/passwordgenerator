0ce7ac4702539c47c0268f4d7c1a38d1484948f2
Amyxcode <107558599+Amyxcode@users.noreply.github.com>
2023-01-19 13:17:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,19 +26,18 @@ consentForm.addEventListener('submit', function(e){
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="loading.svg" class="loading">
-        <p id="upload-text">Uploading your data to the dark web...</p>
     </div>` 
     
     setTimeout(function(){
         document.getElementById('upload-text').innerText = `
-        Making the sale...`
+        `
     }, 1500)
     
     
     setTimeout(function(){
         document.getElementById('modal-inner').innerHTML = `
         <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
-        <p>We just sold the rights to your eternal soul.</p>
+        <p>We appreciate your effort to help us make your experience more enjoyable!</p>
         <div class="idiot-gif">
             <img src="kevin.gif">
         </div>
